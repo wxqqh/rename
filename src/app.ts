eb9385e94176b2747aeab47e2f52a61252b1ef20
wxqqh <wxqqh@qq.com>
2018-11-11 00:29:05 +0800

    增加入口文件, 接口调通

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import * as iconv from "iconv-lite";
+
+import * as debug from "debug";
+
+const LOG = debug(`${__dirname}${__filename}`);
+
+import * as query from "./query";
+
+const firstName = "伍";
+const midName = "芷";
+const lastName = "巧";
+
+const sex = "男";
+
+const birthday = new Date(2018, 10, 6, 17, 20);
+
+// const buf = Buffer.from("ZHjO6dzGx8k=", "base64");
+// const buf = Buffer.from([0xc5, 0xae]);
+// LOG(`buf ${iconv.decode(buf, "GB2312")}`);
+
+const main = async () => {
+    const name = firstName + midName + lastName;
+    LOG(`start ${name} ${sex} ${birthday.toUTCString()}`);
+    const result = await query.CharactersWWWName321Net(name, sex, birthday);
+    LOG(`finish ${result}`);
+};
+
+main();
