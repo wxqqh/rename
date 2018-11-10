b0290cea291a9f79bebdd65cc3f693871bf47de7
wxqqh <wxqqh@qq.com>
2018-11-11 00:27:54 +0800

    增加工具类

---
+++ src/util/DataLoader.ts
@@ -0,0 +1,20 @@
+import * as request from "request";
+import * as iconv from "iconv-lite";
+import * as debug from "debug";
+const LOG = debug(`${__dirname}${__filename}`);
+
+export const DataLoader = (options: request.Options) => {
+    options.encoding = null;
+    return new Promise((resolve, reject) => {
+        request(options, (err, res, body) => {
+            if (!err && res.statusCode === 200) {
+                if (body) {
+                    const result = iconv.decode(Buffer.from(body || [], "binary"), "gb2312").toString();
+                    resolve(result);
+                }
+            } else {
+                reject(err || res.statusCode);
+            }
+        });
+    });
+};
