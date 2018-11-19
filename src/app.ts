1806dbc4bc51ab764892ef25f0f9bc5b83d7a61f
wxqqh <wxqqh@qq.com>
2018-11-19 23:06:56 +0800

    删除不必要的代码

--- src/app.ts
+++ src/app.ts
@@ -11,27 +11,9 @@ import * as disc from "./disc";
 import * as services from "./services";
 
 const firstName = "伍";
-const midName = "芷";
-const lastName = "巧";
-
-const sex = "女";
-
-const birthday = new Date(2018, 10, 6, 17, 20);
-
-// const buf = Buffer.from("ZHjO6dzGx8k=", "base64");
-// const buf = Buffer.from([0xc5, 0xae]);
-// LOG(`buf ${iconv.decode(buf, "GB2312")}`);
 
 const main = async () => {
     try {
-        const name = firstName + midName + lastName;
-        LOG(`start ${name} ${sex} ${birthday.toUTCString()}`);
-
-        // const result = await query.CharactersWWWName321Net(name, sex, birthday);
-        // LOG(`Character finish ${result}`);
-
-        // const nameResult = await query.NameWWWName321Net(name);
-        // LOG(`Name finish ${JSON.stringify(nameResult)}`);
 
         const start = 0;
         const end = 30; // disc.GirlsDouble.length;
