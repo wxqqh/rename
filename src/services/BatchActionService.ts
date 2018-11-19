908d3233d50be4ac88e9a12920a56b0098a51210
wxqqh <wxqqh@qq.com>
2018-11-19 09:32:57 +0800

    增加BatchActionService, 用于批量跑数据

---
+++ src/services/BatchActionService.ts
@@ -0,0 +1,11 @@
+import * as debug from "debug";
+
+const LOG = debug(`${__dirname}${__filename}`);
+
+// import { ReTry } from "../util";
+
+export class BatchActionService<T> {
+    public actoin() {
+        LOG("start");
+    }
+}
