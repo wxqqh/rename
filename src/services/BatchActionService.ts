358a8425904f0bd3beeb889345f12264a3587597
wxqqh <wxqqh@qq.com>
2018-11-19 11:05:10 +0800

    增加百分比日志

--- src/services/BatchActionService.ts
+++ src/services/BatchActionService.ts
@@ -3,7 +3,7 @@ import * as debug from "debug";
 const LOG = debug(`${__dirname}${__filename}`);
 const ERROR = debug(`${__dirname}${__filename}:ERROR`);
 
-import { ReTry } from "../util";
+import { ReTry, Sleep } from "../util";
 
 export class BatchActionService<T> {
     private batchSize: number;
@@ -16,8 +16,10 @@ export class BatchActionService<T> {
         let T: T[] = [];
 
         let currentList = list.splice(0, this.batchSize);
+        let count = 1;
+        const total = Math.ceil(list.length / this.batchSize);
         while (currentList.length > 0) {
-
+            LOG(`BatchActoin current count: ${count} ${(count / total).toFixed(2)}%`);
             const tasks = currentList.map((task) => {
                 return ReTry.doAction(() => action(task)).catch<T>((reason) => {
                     ERROR(`${task} ERROR!!! ${reason}`);
@@ -30,6 +32,8 @@ export class BatchActionService<T> {
 
             T = T.concat(results);
             currentList = list.splice(0, this.batchSize);
+            count++;
+            await Sleep.time(500);
         }
 
         return T;
