49d9ee955a259323809ab79e3d785edb9f1a3c8a
wxqqh <wxqqh@qq.com>
2018-11-19 09:58:18 +0800

    完善批量拉取逻辑

--- src/services/BatchActionService.ts
+++ src/services/BatchActionService.ts
@@ -1,11 +1,37 @@
 import * as debug from "debug";
 
 const LOG = debug(`${__dirname}${__filename}`);
+const ERROR = debug(`${__dirname}${__filename}:ERROR`);
 
-// import { ReTry } from "../util";
+import { ReTry } from "../util";
 
 export class BatchActionService<T> {
-    public actoin() {
-        LOG("start");
+    private batchSize: number;
+    constructor(batchSize: number = 10) {
+        this.batchSize = batchSize;
+    }
+    public async BatchActoin(list: string[], action: (...args: any[]) => Promise<T>) {
+        LOG("BatchActoin start");
+        // tslint:disable-next-line:no-shadowed-variable
+        let T: T[] = [];
+
+        let currentList = list.splice(0, this.batchSize);
+        while (currentList.length > 0) {
+
+            const tasks = currentList.map((task) => {
+                return ReTry.doAction(() => action(task)).catch<T>((reason) => {
+                    ERROR(`${task} ERROR!!! ${reason}`);
+                    const errorT: any = null;
+                    return errorT;
+                });
+            });
+
+            const results = await Promise.all(tasks);
+
+            T = T.concat(results);
+            currentList = list.splice(0, this.batchSize);
+        }
+
+        return T;
     }
 }
