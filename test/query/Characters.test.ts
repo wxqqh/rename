ce76af5b41ccbaf943ba51c5c098bf3a39cd7c79
wxqqh <wxqqh@qq.com>
2018-11-19 23:06:48 +0800

    增加query/Characters的单元测试用例

---
+++ test/query/Characters.test.ts
@@ -0,0 +1,19 @@
+import * as assert from "assert";
+
+import * as query from "../../src/query";
+import * as debug from "debug";
+
+const LOG = debug(`${__filename}`);
+
+const name = "伍芷巧";
+const sex = "女";
+const birthday = new Date(2018, 10, 6, 17, 20);
+
+describe(__filename, () => {
+    it("query CharactersWWWName321Net response success", async () => {
+        const result = await query.CharactersWWWName321Net(name, sex, birthday);
+
+        assert.notEqual(result, null);
+        assert.notEqual(result.length, 0);
+    });
+});
